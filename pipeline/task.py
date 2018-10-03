d38bc1c8a14a86564e0fd57e18773e82e72b64f0
sfujiwara <shuhei.fujiwara@gmail.com>
2018-10-04 01:31:39 +0900

    nice commit.

--- pipeline/task.py
+++ pipeline/task.py
@@ -15,13 +15,18 @@ class TemplateOptions(PipelineOptions):
 
 class CopyFile(beam.DoFn):
 
-    def __init__(self, gdrive_directory_id):
+    def __init__(self, gdrive_directory_id, service_account_file):
         self.gdrive_directory_id = gdrive_directory_id
+        self.service_account_file = service_account_file
 
     def process(self, element):
         import tensorflow as tf
+        from google.oauth2 import service_account
+        from googleapiclient.discovery import build
+        from apiclient import http
         import os
         gcs_file_path, gdrive_file_name = element.split(",")
+        gcs_file_path, gdrive_file_name = gcs_file_path.encode("utf-8"), gdrive_file_name.encode("utf-8")
         # Read file in Cloud Storage
         with tf.gfile.Open(gcs_file_path, "rb") as f:
             file_content = f.read()
@@ -31,6 +36,26 @@ class CopyFile(beam.DoFn):
         tf.gfile.MakeDirs("tmp")
         with tf.gfile.Open("tmp/{}".format(gdrive_file_name), "w") as f:
             f.write(file_content)
+        # Download service account file
+        print(self.service_account_file.get())
+        with tf.gfile.Open(self.service_account_file.get(), "rb") as f:
+            file_content = f.read()
+        with tf.gfile.Open("service_account.json", "w") as f:
+            f.write(file_content)
+        # Upload file to Google Drive
+        scopes = ["https://www.googleapis.com/auth/drive.file"]
+        credentials = service_account.Credentials.from_service_account_file(
+            "service_account.json",
+            scopes=scopes
+        )
+        service = build("drive", "v3", credentials=credentials, cache_discovery=False)
+        media_body = http.MediaFileUpload("tmp/{}".format(gdrive_file_name))
+        body = {
+            "name": gdrive_file_name,
+            "parents": [self.gdrive_directory_id.get()],
+        }
+        print(body)
+        file_ = service.files().create(body=body, fields="id", media_body=media_body).execute()
 
 
 def main():
@@ -40,10 +65,8 @@ def main():
 
     p = beam.Pipeline(options=options)
 
-    pipeline_input = ["hello", "world"]
-
     (p | "Read" >> beam.io.ReadFromText(options.input_csv)
-       | "Write" >> beam.ParDo(CopyFile(options.gdrive_directory_id)))
+       | "Write" >> beam.ParDo(CopyFile(options.gdrive_directory_id, options.service_account_file)))
 
     print("hello")
     p.run()
